111da64097cc772e197ba365ab9c7d22a4605260
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-24 07:16:36 -0700

    removed 0-main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - casuses an infinite loop
+ * return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 *while (i < 10)
+	 *{
+	 *       putchar(i);
+	 *}
+	 */
+       printf("Infinite loop avoided! \\o/\n");
+	
+       return (0);
+}
