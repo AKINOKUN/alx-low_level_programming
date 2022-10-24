3a83a308b9e3fc4fe7333cb50ba3b45a5bbc3f3f
AKINOKUN <113610880+AKINOKUN@users.noreply.github.com>
2022-10-24 15:57:21 +0100

    added puts main rev string print files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates to 98
+ * @n: integer input pointer
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
