1cad948b03aae114190418bedcd7c8a9d231570f
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-26 10:44:29 -0700

    9 ptinz

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)  
-{
-  int d;
-  
-  for (d = '0'; d <= '9'; d++)
-    {
-      putchar(d); 
-      if (d != '9')
+int main(void)
 	{
-    putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
+	int a;
 
-  return (0);  
-}
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+	}
+
