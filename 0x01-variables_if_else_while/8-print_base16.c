b23beb2691e219637cd775a4b08f220d75d5908b
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-26 10:23:29 -0700

    8 printz

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
-int main(void)  
+int main(void)
 {
-  int d;
-  char low;
-    
-  for (d = '0'; d <= '9'; d++)  
-    putchar(d);
-  for (low = 'a'; low <= 'f'; low++)
-    putchar(low);
-  putchar('\n');
+	char c;
 
-  return (0);  
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
+
