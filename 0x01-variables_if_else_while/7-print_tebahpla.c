8dfb3d8ba4d39e6118b08d5d66b776de912fad43
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-26 09:50:05 -0700

    corrected

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
- 	 char c;
+	char c;
 
-	 c = 'z';
-	 while
-		 (c >= 'a') {
-			 putchar(c);
-			 c--;
-		 }
-	 putchar('\n');
-	 return(0);
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
 }
