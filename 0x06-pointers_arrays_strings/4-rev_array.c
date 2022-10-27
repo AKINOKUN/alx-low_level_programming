c5369281247743f8346821c26143e00204079f49
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-27 09:05:16 -0700

    array 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,7 +6,6 @@
  * @n: number of elements of the array
  * Return: rev
  */
-
 void reverse_array(int *a, int n)
 {
 	int i;
@@ -16,7 +15,7 @@ void reverse_array(int *a, int n)
 	{
 		n--;
 		j = a[i];
-		a(i) = a[n];
+		a[i] = a[n];
 		a[n] = j;
 	}
 }
