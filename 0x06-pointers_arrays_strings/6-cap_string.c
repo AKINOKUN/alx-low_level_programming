80ae023151c9e05107d21554d0073e0536b7733f
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-27 08:57:03 -0700

    string 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - capitalizes words
- * @str : string
+ * @str: string
  * Return: pointer to string
  */
 char *cap_string(char *str)
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			I++;
+			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
@@ -28,6 +28,7 @@ char *cap_string(char *str)
 		    str[i - 1] == '}' ||
 		    i == 0)
 			str[i] -= 32;
+
 		i++;
 	}
 
