91317ef86319da646c23805818582f2893ceea1f
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-27 15:39:52 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -19,7 +19,7 @@ char *rot13(char *str)
 			     'q', 'r', 's', 't', 'u', 'v',
 			     'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		             'T', 'U', 'V', 'W', 'X', 'Y',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
 			     'F', 'G', 'H', 'I', 'J', 'K',
 			     'L', 'M', 'n', 'o', 'p', 'q',
@@ -37,7 +37,9 @@ char *rot13(char *str)
 				break;
 			}
 		}
+
 		indx1++;
 	}
-	return (str):
+
+	return (str);
 }
