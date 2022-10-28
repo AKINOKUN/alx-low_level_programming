644fe3c7c7b975e14ce073fce7eaddf53c75a734
AKINOKUN <akintundeifeoluwa03@gmail.com>
2022-10-28 06:40:12 -0700

    fixed main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,9 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *a);
 int _putchar(char c);
+void print_number(int n);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
